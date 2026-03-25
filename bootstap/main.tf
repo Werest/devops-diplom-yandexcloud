09455a744944eb2f3790384047d2434968b25ac6
Konstantin Chigridov <werest54@rambler.ru>
2026-03-25 21:21:52 +0300

    Создание облачной инфраструктуры (готово)

---
+++ bootstap/main.tf
@@ -0,0 +1,37 @@
+# Создание сервисного аккаунта
+resource "yandex_iam_service_account" "terraform" {
+  name        = "terraform-sa-diploma"
+  description = "Service account for Terraform"
+}
+
+# Назначение ролей (минимально необходимые)
+resource "yandex_resourcemanager_folder_iam_member" "editor" {
+  folder_id = var.yc_folder_id
+  role      = "editor"
+  member    = "serviceAccount:${yandex_iam_service_account.terraform.id}"
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "images_puller" {
+  folder_id = var.yc_folder_id
+  role      = "container-registry.images.puller"
+  member    = "serviceAccount:${yandex_iam_service_account.terraform.id}"
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "images_pusher" {
+  folder_id = var.yc_folder_id
+  role      = "container-registry.images.pusher"
+  member    = "serviceAccount:${yandex_iam_service_account.terraform.id}"
+}
+
+# Статический ключ доступа для работы с Object Storage
+resource "yandex_iam_service_account_static_access_key" "terraform_sa_key" {
+  service_account_id = yandex_iam_service_account.terraform.id
+  description        = "Static access key for Terraform to manage bucket"
+}
+
+# Создание бакета для хранения стейта
+resource "yandex_storage_bucket" "tfstate" {
+  bucket     = "tfstate-bucket-${var.bucket_suffix}"
+  access_key = yandex_iam_service_account_static_access_key.terraform_sa_key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.terraform_sa_key.secret_key
+}
---
+++ main/main.tf
@@ -0,0 +1,107 @@
+# Создаём VPC, подсети в трёх зонах, группы безопасности,
+# ВМ для мастер-ноды и воркер-нод (прерываемые для воркеров)
+
+# Сеть
+resource "yandex_vpc_network" "k8s_net" {
+  name = "k8s-network"
+}
+
+# Подсети
+resource "yandex_vpc_subnet" "subnet_a" {
+  name           = "subnet-a"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.k8s_net.id
+  v4_cidr_blocks = ["10.1.0.0/24"]
+}
+
+resource "yandex_vpc_subnet" "subnet_b" {
+  name           = "subnet-b"
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.k8s_net.id
+  v4_cidr_blocks = ["10.2.0.0/24"]
+}
+
+# Группа безопасности для доступа к API и ssh
+resource "yandex_vpc_security_group" "k8s_sg" {
+  name       = "k8s-security-group"
+  network_id = yandex_vpc_network.k8s_net.id
+
+  ingress {
+    protocol       = "TCP"
+    port           = 22
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    description    = "SSH"
+  }
+
+  ingress {
+    protocol       = "TCP"
+    port           = 6443
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    description    = "Kubernetes API"
+  }
+}
+
+# Мастер-нода (непрерываемая, для стабильности)
+resource "yandex_compute_instance" "master" {
+  name        = "k8s-master"
+  platform_id = "standard-v2"
+  zone        = "ru-central1-a"
+
+  resources {
+    cores  = 2
+    memory = 4
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8e5jmcvep85j33nt0e" # Ubuntu 24.04 LTS
+      size     = 20
+      type     = "network-hdd"
+    }
+  }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.subnet_a.id
+    security_group_ids = [yandex_vpc_security_group.k8s_sg.id]
+    nat                = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+# Воркер-ноды (прерываемые)
+resource "yandex_compute_instance" "worker" {
+  count       = 2
+  name        = "k8s-worker-${count.index}"
+  platform_id = "standard-v2"
+  zone        = count.index == 0 ? "ru-central1-a" : "ru-central1-b" # распределим по зонам
+
+  resources {
+    cores  = 2
+    memory = 4
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8e5jmcvep85j33nt0e"
+      size     = 20
+      type     = "network-hdd"
+    }
+  }
+
+  network_interface {
+    subnet_id          = count.index == 0 ? yandex_vpc_subnet.subnet_a.id : yandex_vpc_subnet.subnet_b.id
+    security_group_ids = [yandex_vpc_security_group.k8s_sg.id]
+    nat                = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+
+  scheduling_policy {
+    preemptible = true
+  }
+}
